79f0844008f10324301ed4e9bf9f46f6809e3190
karan <karanc4143@gmail.com>
2024-07-31 01:11:23 +0530

    new update

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,6 @@
 // array
 
-const myArr = [0, 1, 2, 3, 4, 5]
+const myArr = [11, 1, 2, 0,3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
@@ -11,27 +11,31 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(6)
 // myArr.push(7)
 // myArr.pop()
-
-// myArr.unshift(9)
+// {
+//     myArr.unshift(9, 3, 5, 7, 4)
+//     console.log(myArr);
+// }
 // myArr.shift()
+// myArr.shift()
+// console.log(myArr);
 
 // console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.indexOf(1));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log( newArr);
+// console.log(typeof myArr);
+// console.log( typeof newArr);
 
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)
--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,23 +9,29 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+// const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
 
+Array.prototype.sum = function() {
+    return this.reduce((acc, curr) => acc + curr, 0);
+};
+
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// console.log(another_array);
+// console.log(another_array.sum());
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Hitesh"))
+console.log(Array.from(457457))
+// console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
